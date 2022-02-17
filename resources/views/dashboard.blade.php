b73aad7200ccde05922e2c94552a0260667c7522
Muhammed Swalih <swalihvmbazar@gmail.com>
2022-02-17 20:05:59 +0530

    Design dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,15 @@
 <x-app-layout>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-                Welcome {{ auth()->user()->name }}
-            </div>
-        </div>
+    <div class="container mt-5 w-50 border p-3">
+        <h5>Welcome {{ auth()->user()->name }}</h5>
+        <table class="table mt-5">
+            <tr>
+                <th>YOUR ID</th>
+                <td>{{ auth()->user()->email }}</td>
+            </tr>
+            <tr>
+                <th>YOUR BALANCE</th>
+                <td>12000.00</td>
+            </tr>
+        </table>
     </div>
 </x-app-layout>
