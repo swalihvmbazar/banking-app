3897da427aa94bd638d9c5d656c4fcdf569c9d48
Muhammed Swalih <swalihvmbazar@gmail.com>
2022-02-17 20:05:06 +0530

    Make navigation links

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <div class="d-flex justify-content-center align-items-center w-100 ">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item d-flex align-items-center mr-2 {{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                    <i class="fa fa-home"></i>
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item d-flex align-items-center">
+                    <i class="fa fa-upload"></i>
+                    <a class="nav-link" aria-current="page" href="#">Deposit</a>
+                </li>
+
+                <li class="nav-item d-flex align-items-center">
+                    <i class="fa fa-download"></i>
+                    <a class="nav-link" aria-current="page" href="#">Withdraw</a>
+                </li>
+
+                <li class="nav-item d-flex align-items-center">
+                    <i class="fa fa-arrow-right"></i>
+                    <a class="nav-link" aria-current="page" href="#">Transfer</a>
+                </li>
+              
+                <li class="nav-item d-flex align-items-center">
+                    <i class="fa fa-file"></i>
+                    <a class="nav-link" aria-current="page" href="#">Statement</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,56 +7,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Banking App') }}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                    
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <div class="container">
+                <a class="navbar-brand" href="{{ route('dashboard') }}">
+                    <h3>ABC BANK</h3>
+                </a>
+                <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                            this.closest('form').submit();">Logout</a>
+                                    this.closest('form').submit();">Logout</a>
                         </form>
                     </li>
                 </ul>
-
             </div>
-        </div>
-    </nav>
+        </nav>
+    </header>
+    @include('components.navigation')
     {{ $slot }}
 </body>
 
