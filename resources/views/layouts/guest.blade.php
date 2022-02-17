505a706a66faf959d4360efd30011b63f509e2d4
Muhammed Swalih <swalihvmbazar@gmail.com>
2022-02-17 12:22:34 +0530

    Link app css in guest app layouts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Banking App') }}</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     {{ $slot }}
